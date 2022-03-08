acd347e1ab43ed1815342f828c21fbd927002c07
ManhAn9077 <94302143+ManhAn9077@users.noreply.github.com>
2022-03-08 09:31:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Lớp 12D4';
+const giftUrl = 'https://www.tiktok.com/@huanose86/video/7070130459417660699?is_copy_url=1&is_from_webapp=v1&q=8%2F3&t=1646704928405';
+const eventName = 'Chúc Mừng 08/03';
+const titleCard = 'Toàn thể phái nữ lớp 12D4';
+const contentCard = 'Chúc các bạn nữ 12D4 có luôn tràn ngập liền vui và hạnh phúc. Mong điều đẹp nhất sẽ đến với các bạn nữ trong hôm nay và chúc các bạn đỗ được nguyện vọng mình đặt ra';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'lớp 12d4.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
